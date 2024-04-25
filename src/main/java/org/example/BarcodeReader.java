ac7d240beff1347aa6ab3b21d1c0d4f7100d050d
kikswosil <kikswosil@wp.pl>
2024-04-25 12:40:56 +0200

    minor changes in qrcode displaying

--- src/main/java/org/example/BarcodeReader.java
+++ src/main/java/org/example/BarcodeReader.java
@@ -6,11 +6,19 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.Dimension;
 import java.awt.image.BufferedImage;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class BarcodeReader implements Runnable {
     private final Webcam webcam;
     private final JFrame window;
     private final int WINDOW_HEIGHT = 800, WINDOW_WIDTH = 1200;
+
+    private String prevDecodedValue = "";
+
+    private DateFormat format = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
     public BarcodeReader() {
         // webcam setup.
         this.webcam = Webcam.getDefault();
@@ -24,12 +32,15 @@ public class BarcodeReader implements Runnable {
     @Override
     public void run() {
         this.webcam.open();
+        System.out.println("--------------------------------------------------");
         while(this.window.isShowing()) loop();
+        System.out.println("--------------------------------------------------");
         this.webcam.close();
     }
 
     private void loop() {
         BufferedImage image = this.webcam.getImage();
+
         if(image == null || !this.window.isShowing()) return;
         this.window.getGraphics().drawImage(
                 image.getScaledInstance(this.window.getWidth(), this.window.getHeight(), Image.SCALE_SMOOTH),
@@ -37,7 +48,11 @@ public class BarcodeReader implements Runnable {
                 0,
                 null
         );
+
         String result = BitmapDecoder.decode(ImageToBitmapConverter.convert(image)).orElse("");
-        if(!result.isEmpty()) System.out.println(result);
+        String time = this.format.format(new Date());
+
+        if(!result.isEmpty() && !result.equals(this.prevDecodedValue)) System.out.println(String.format("|> %s >> %s", time, result));
+        this.prevDecodedValue = result;
     }
 }
